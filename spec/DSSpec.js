83f3e0fe351cb47b2d6eb6442557e8b82b18f42d
Jatin <jati.arora@gmail.com>
2016-03-15 18:56:58 +0530

    library V.1

---
+++ spec/DSSpec.js
@@ -0,0 +1,219 @@
+describe("Data Structure Liabrary", function () {
+    it("Create LinkedList with initial value", function () {
+        //Arrange
+
+        //Act
+        var list = $d.linkedlist(4);
+        //Assert
+        expect(list.next()).toBe(4);
+    });
+    it("Create LinkedList without initial value", function () {
+        //Arrange
+
+        //Act
+        var list = $d.linkedlist();
+        list.addToLast(4);
+        list.addAtFirst(5);
+
+        //Assert
+        expect(list.next()).toBe(5);
+    });
+    it("Create LinkedList with keyword", function () {
+        //Arrange
+
+        //Act
+        var list = $d(":ll");
+        list.addToLast(4);
+        list.addAtFirst(5);
+
+        //Assert
+        expect(list.next()).toBe(5);
+        expect(list.size()).toBe(2);
+    });
+    it("Create Stack with keyword", function () {
+        //Arrange
+
+        //Act
+        var stack = $d(':st');
+        stack.push(44);
+        stack.push(55);
+        stack.push(66);
+
+        //Assert
+        expect(stack.pop()).toBe(66);
+        //expect(stack.pop()).toBe(55);
+
+    });
+    it("Create Stack and check teh size", function () {
+        //Arrange
+
+        //Act
+        var stack = $d(':st');
+        stack.push(44);
+        stack.push(55);
+        stack.push(66);
+
+        //Assert
+        expect(stack.size()).toBe(3);
+
+    });
+    it("Create Stack and check the peek", function () {
+        //Arrange
+
+        //Act
+        var stack = $d(':st');
+        stack.push(44);
+        stack.push(55);
+        stack.push(66);
+
+        //Assert
+        expect(stack.peek()).toBe(66);
+        expect(stack.peek()).toBe(66);
+
+    });
+    it("Create Queue and check the enqueue", function () {
+        //Arrange
+
+        //Act
+        var queue = $d(':qu');
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        //Assert
+        expect(queue.size()).toBe(3);
+        expect(queue.dequeue()).toBe(1);
+        expect(queue.size()).toBe(2);
+
+    });
+    it("Create binary search tree and add items", function () {
+        //Arrange
+
+        //Act
+        var bt = $d(':bst');
+        bt.add(5);
+        bt.add(2);
+        bt.add(10);
+        bt.add(10);
+        bt.add(91);
+        bt.add(11);
+        bt.add(1);
+        bt.add(311);
+        var list = bt.inorder();
+
+        // console.log(list);
+        //Assert
+        expect(list.length).toBe(8);
+        // expect(stack.peek()).toBe(66);
+
+    });
+    it("Create binary search tree and add items with sorted citeria", function () {
+        //Arrange
+        var Point = function (x, y) {
+            this.x = x;
+            this.y = y;
+        };
+
+        function comparator(p1, p2) {
+            // console.log("COMP:  " + p1.x + "    " + p2.x);
+            var out = -1;
+            if (p1.x == p2.x && p1.y === p2.y)
+                out = 0;
+            if (p1.x > p2.x && p1.y > p2.y)
+                out = 1;
+            //console.log("out:" + out);
+            return out;
+        };
+
+
+        //Act
+        var bt = $d(':bst', comparator);
+
+        bt.add(new Point(5, 5));
+        bt.add(new Point(2, 2));
+        bt.add(new Point(10, 10));
+        bt.add(new Point(10, 10));
+        bt.add(new Point(91, 91));
+        bt.add(new Point(11, 11));
+        bt.add(new Point(8, 5));
+        bt.add(new Point(1, 1));
+
+        var list = bt.inorder();
+        /* for (var p in list) {
+             console.log(list[p].x + ",  " + list[p].y);
+         }*/
+        //Assert
+        expect(list.length).toBe(8);
+        // expect(stack.peek()).toBe(66);
+
+    });
+    it("Create binary search tree and add string", function () {
+        //Arrange
+        var Point = function (x, y) {
+            this.x = x;
+            this.y = y;
+        };
+
+        function comparator(p1, p2) {
+            // console.log("COMP:  " + p1.x + "    " + p2.x);
+            var out = -1;
+            if (p1.x == p2.x && p1.y === p2.y)
+                out = 0;
+            if (p1.x > p2.x && p1.y > p2.y)
+                out = 1;
+            //  console.log("out:" + out);
+            return out;
+        };
+
+
+        //Act
+        var bt = $d(':bst');
+
+        bt.add('31');
+        bt.add('42');
+        bt.add('13');
+        bt.add('45');
+        bt.add('35');
+
+        var list = bt.inorder();
+        /*for (var p in list) {
+            console.log(list[p]);
+        }*/
+        //Assert
+        expect(list.length).toBe(5);
+        // expect(stack.peek()).toBe(66);
+
+    });
+
+    it("Create binary search tree and remove items", function () {
+        //Arrange
+
+        //Act
+        var bt = $d(':bst');
+        bt.add(10);
+        bt.add(7);
+        bt.add(15);
+        bt.add(5);
+        bt.add(9);
+        bt.add(12);
+        bt.add(20);
+        bt.add(25);
+        console.log(bt.inorder());
+        var list = bt.remove(7);
+        console.log("after remove: " + bt.inorder());
+
+        /* var list = bt.remove(7);
+        console.log("after remove: " + bt.inorder());*/
+        // var list = bt.remove(12);
+        //console.log("after remove: " + bt.inorder());
+
+        var list = bt.remove(10);
+        console.log("after remove: " + bt.inorder());
+
+
+        //Assert
+        //expect(list.length).toBe(8);
+        // expect(stack.peek()).toBe(66);
+
+    });
+})
---
+++ src/DS.js
@@ -0,0 +1,266 @@
+var $d = (function () {
+    var ds = function (input, additionFn) {
+        //console.log(input);
+        if (input === ':ll') {
+            return ds.linkedlist();
+        } else if (input === ":st") {
+            return ds.stack();
+        } else if (input === ":qu") {
+            return ds.queue();
+        } else if (input === ":bst") {
+            return ds.binarySearchTree(additionFn);
+        }
+    };
+
+    function Node(val, next, previous) {
+        this.val = val;
+        this.next = next;
+        this.previous = previous;
+    };
+
+    function LinkedList(val) {
+        if (val !== undefined) {
+            this.root = new Node(val);
+            this.nextNode = this.root;
+        }
+    };
+
+    LinkedList.prototype.addToLast = function (val) {
+        var newNode = new Node(val);
+        if (this.root === undefined) {
+            this.root = newNode;
+        } else {
+            var tmpNode = this.root;
+            while (tmpNode.next !== undefined) {
+                tmpNode = tmpNode.next;
+            }
+            tmpNode.next = newNode;
+        }
+        this.nextNode = this.root;
+        return this;
+    };
+
+    LinkedList.prototype.addAtFirst = function (val) {
+        var newNode = new Node(val);
+        newNode.next = this.root;
+        this.root = newNode;
+        this.nextNode = this.root;
+        return this;
+    };
+
+    LinkedList.prototype.moveToFirst = function () {
+        this.nextNode = this.root;
+    };
+
+    LinkedList.prototype.next = function () {
+        var value;
+        if (this.nextNode !== undefined) {
+            value = this.nextNode.val;
+            this.nextNode = this.nextNode.next;
+        }
+        return value;
+    };
+    LinkedList.prototype.size = function () {
+        var size = 0;
+        var tmpNode = this.root;
+        while (tmpNode !== undefined) {
+            tmpNode = tmpNode.next;
+            size++;
+        }
+        return size;
+    };
+
+    function Stack() {
+        this.stackArray = [];
+    };
+    Stack.prototype.push = function (val) {
+        if (val !== undefined) {
+            this.stackArray.push(val);
+        }
+    };
+
+    Stack.prototype.pop = function () {
+        return this.stackArray.splice(this.stackArray.length - 1, 1)[0];
+    };
+
+    Stack.prototype.peek = function () {
+        return this.stackArray[this.stackArray.length - 1];
+    };
+
+    Stack.prototype.size = function () {
+        return this.stackArray.length;
+    };
+
+    function Queue() {
+        this.queueArray = [];
+    };
+    Queue.prototype.enqueue = function (val) {
+        this.queueArray.push(val);
+    };
+    Queue.prototype.dequeue = function () {
+        return this.queueArray.splice(0, 1)[0];
+    };
+    Queue.prototype.peek = function () {
+        return this.queueArray[0];
+    };
+    Queue.prototype.size = function () {
+        return this.queueArray.length;
+    };
+
+    function BinarySearchTree(comparatorFn) {
+        this.root;
+        this.comparatorFn = comparatorFn;
+    };
+
+    BinarySearchTree.prototype.add = function (val) {
+        var newNode = new Node(val);
+        var compFn = this.comparatorFn;
+        if (this.root === undefined) {
+            this.root = newNode;
+        } else {
+            addNewNode(this.root, newNode);
+        };
+
+        function addNewNode(node, newNode) {
+            if (compare(node.val, newNode.val) > 0) {
+                if (node.previous === undefined) {
+                    node.previous = newNode;
+                } else {
+                    addNewNode(node.previous, newNode);
+                }
+            } else {
+                if (node.next === undefined) {
+                    node.next = newNode;
+                } else {
+                    addNewNode(node.next, newNode);
+                }
+            }
+        };
+
+        function compare(var1, var2) {
+            if (compFn !== undefined) {
+                return compFn(var1, var2);
+            } else {
+                return var1 > var2 ? 1 : (var1 < var2 ? -1 : 0);
+            }
+        }
+
+    };
+
+    BinarySearchTree.prototype.delete = function (remVal) {
+        var rootNode = this.root;
+        var compFn = this.compareFn;
+
+        var pNode, sNode;
+
+        if (equals(rootNode.val, remVal)) {
+            // ROOT NEED TO BE REMOVED   
+            sNode = rootNode;
+            console.log("ITS A ROOT NODE : " + sNode.val);
+        } else {
+            preorder(rootNode);
+        }
+
+        function equals(val1, val2) {
+            if (compFn === undefined) {
+                return val1 === val2 ? true : false;
+            } else {
+                return compFn(val1, val2) === 0;
+            }
+        };
+
+        // considering it always has next child
+        function sucessor(node) {
+            var sucessorNode = node.next;
+            while (sucessorNode.previous !== undefined) {
+                sucessorNode = sucessorNode.previous;
+            }
+            return sucessorNode;
+        }
+
+        function preorder(node) {
+            if (node === undefined)
+                return false;
+            if (node.previous !== undefined && equals(node.previous.val, remVal)) {
+                pNode = node;
+                sNode = node.previous;
+                return true;
+            } else if (node.next !== undefined && equals(node.next.val, remVal)) {
+                pNode = node;
+                sNode = node.next;
+                return true;
+            };
+            return preorder(node.previous) || preorder(node.next);
+        };
+
+        console.log("parentNode: " + (pNode !== undefined ? pNode.val : "undefined") + " Searched Node:" + (sNode == undefined ? "undefined" : sNode.val));
+
+        if (sNode !== undefined) {
+            if (sNode.previous !== undefined && sNode.next !== undefined) {
+                var sucessorNode = sucessor(sNode);
+                this.delete(sucessorNode.val);
+
+                if (pNode !== undefined) {
+                    if (equals(pNode.previous.val, sNode.val)) {
+                        pNode.previous = sucessorNode;
+                    } else {
+                        pNode.next = sucessorNode;
+                    }
+                } else {
+                    this.root = sucessorNode;
+                }
+                sucessorNode.previous = sNode.previous;
+                sucessorNode.next = sNode.next;
+
+            } else if (sNode.previous !== undefined || sNode.next !== undefined) {
+                if (sNode.previous !== undefined) {
+                    pNode.previous = sNode.previous;
+                } else {
+                    pNode.next = sNode.next;
+                }
+            } else {
+                if (pNode !== undefined) {
+                    if (pNode.previous !== undefined && equals(pNode.previous.val, sNode.val)) {
+                        pNode.previous = undefined;
+                    } else {
+                        pNode.next = undefined;
+                    }
+                } else {
+                    this.root = undefined;
+                }
+            }
+        }
+    };
+
+    BinarySearchTree.prototype.inorder = function () {
+        var inorderRes = [];
+
+        function inorderTraversal(node) {
+            if (node === undefined) {
+                return;
+            }
+            inorderTraversal(node.previous);
+            inorderRes.push(node.val);
+            inorderTraversal(node.next);
+        };
+        inorderTraversal(this.root);
+        return inorderRes;
+    };
+
+
+    ds.queue = function () {
+        return new Queue();
+    };
+    ds.stack = function () {
+        return new Stack();
+    };
+
+    ds.linkedlist = function (val) {
+        return new LinkedList(val);
+    };
+    ds.binarySearchTree = function (compFn) {
+        return new BinarySearchTree(compFn);
+    };
+    return ds;
+
+})();
